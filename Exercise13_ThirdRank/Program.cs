d5e9dcfcffbbea8b3127ad29b6221f8ae7f2fabb
Egor Gasanov <Gasanov.EV11@ya.ru>
2023-06-20 02:43:16 +0300

    Exerscise15_WeekDay

---
+++ Exercise13_ThirdRank/Program.cs
@@ -0,0 +1,18 @@
+﻿﻿Console.Clear();
+Console.WriteLine("Введите  число ");
+string userInput = Console.ReadLine()??"";
+int number = int.Parse(userInput);
+if (number<=100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else if (number > 999)
+{
+int a1 = number / 10;
+int a2 = number % 10;
+
+int result = a1*10-a2;
+
+
+Console.WriteLine($"{number} -> {result}");
+}
---
+++ Exerscise15_WeekDay/Program.cs
@@ -0,0 +1,68 @@
+﻿Console.Clear();
+Console.WriteLine("Введите  число целое число от 1 до 7 ");
+string userInput = Console.ReadLine() ?? "";
+int number = int.Parse(userInput);
+
+if (number<1 | number>7)
+{
+    Console.WriteLine($"Ошибка ввода. Число {number} не является днем недели. ");
+}
+
+else switch (number)
+    {
+case 1:
+Console.WriteLine("Нет!День рабочий.");
+break;
+
+case 2:
+Console.WriteLine("Нет!День рабочий.");
+break;
+
+case 3:
+Console.WriteLine("Нет!День рабочий.");
+break;
+
+case 4:
+Console.WriteLine("Нет!День рабочий.");
+break;
+
+case 5:
+Console.WriteLine("Нет!День рабочий.");
+break;
+
+case 6:
+Console.WriteLine("Да!Сегодня выходной!");
+break;
+
+case 7:
+Console.WriteLine("Да!Сегодня выходной!");
+break;
+
+
+}
+
+
+
+
+
+
+
+
+
+/*if (number<1 | number>7)
+{
+    Console.WriteLine($"Ошибка ввода. Число {number} не является днем недели ");
+}
+else 
+{
+
+}
+{
+int a1 = number / 100;
+int a2 = number / 10;
+
+int result = a1*10-a2;
+
+
+Console.WriteLine($"{number} -> {result}");
+}*/
