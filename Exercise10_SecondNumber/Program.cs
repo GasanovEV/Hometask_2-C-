c0198ebf367ae2a69afd035e9951c60c2c3d9e12
Egor Gasanov <Gasanov.EV11@ya.ru>
2023-06-20 01:17:57 +0300

    Exercise10_SecondNumber

---
+++ Exercise10_SecondNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿﻿Console.Clear();
+Console.WriteLine("Введите трехзначное число ");
+string userInput = Console.ReadLine()??"";
+int number = int.Parse(userInput);
+if (100>number | number>999)
+{
+    Console.WriteLine("Ошибка ввода");
+}
+else
+{
+int a1 = number / 100;
+int a2 = number / 10;
+
+int result = a1*10-a2;
+
+
+Console.WriteLine($"{number} -> {result}");
+}
